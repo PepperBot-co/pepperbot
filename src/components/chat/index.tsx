3529c0cb614c061af8583fde18775c516c19c80a
Ahmad Taha <ahmad.taha@maf.ae>
2023-05-24 13:54:59 +0300

    initial chat component design

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,6 +1,27 @@
+import ChatFooter from "../chat-footer";
+
+/* eslint-disable react/no-unescaped-entities */
 const Chat: React.FC = () => {
   return (
-    <div className="h-[calc(100vh-65px)] border-l border-base-300 bg-base-100 p-5"></div>
+    <div className="h-[calc(100vh-65px)] border-l border-base-300 bg-base-100 p-5">
+      <div
+      // className="fixed flex w-full justify-between bg-green-100"
+      // style="bottom: 0px;"
+      >
+        <div className="chat chat-start">
+          <div className="chat-bubble">
+            It's over Anakin, <br />I have the high ground.
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-bubble">You underestimate my power!</div>
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 mx-auto">
+        <ChatFooter />
+      </div>
+    </div>
   );
 };
 
