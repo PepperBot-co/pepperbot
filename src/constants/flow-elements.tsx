58cfa3d868f5b645b965380280013c92956095fc
Laith Younes <laithyounesy@gmail.com>
2023-06-04 01:56:20 +0300

    added node configuration data

--- src/constants/flow-elements.tsx
+++ src/constants/flow-elements.tsx
@@ -9,11 +9,16 @@ export const initialNodes: Node[] = [
       icon: <Bolt />,
       description:
         "Write your into. Add '[reason]' or '[name]' for your personal touch.",
+      nodeConfigs: {
+        type: "QA",
+        message: "Hi, How is your day going so far?",
+        expectedAnswer: "",
+      },
     },
     position: { x: 0, y: 0 },
     type: "intro",
     selected: false,
-    selectable: false,
+    selectable: true,
   },
   {
     id: "2",
@@ -21,24 +26,51 @@ export const initialNodes: Node[] = [
       label: "Message",
       icon: <ChatBubble />,
       description: "Craft a query that requires responses in unformatted text.",
+      nodeConfigs: {
+        type: "QA",
+        message: "What have you will be doing today?",
+        expectedAnswer: "Ticket number is required",
+      },
     },
     position: { x: 0, y: 300 },
     type: "workflow",
     selected: false,
-    selectable: false,
+    selectable: true,
   },
   {
     id: "3",
+    data: {
+      label: "Message",
+      icon: <ChatBubble />,
+      description: "Craft a query that requires responses in unformatted text.",
+      nodeConfigs: {
+        type: "QA",
+        message: "What have you been doing since yesterday?",
+        expectedAnswer: "Ticket number is required",
+      },
+    },
+    position: { x: 0, y: 600 },
+    type: "outro",
+    selected: false,
+    selectable: true,
+  },
+  {
+    id: "4",
     data: {
       label: "Outro",
       icon: <Bolt />,
       description:
         "Write a friendly closing message, blending appreciation and your unique touch.",
+      nodeConfigs: {
+        type: "QA",
+        message: "Thank you!",
+        expectedAnswer: "",
+      },
     },
-    position: { x: 0, y: 600 },
+    position: { x: 0, y: 900 },
     type: "outro",
     selected: false,
-    selectable: false,
+    selectable: true,
   },
 ];
 
@@ -53,6 +85,12 @@ export const initialEdges: Edge[] = [
     id: "2=>3",
     source: "2",
     target: "3",
+    type: "workflow",
+  },
+  {
+    id: "3=>4",
+    source: "3",
+    target: "4",
     type: "workflow",
   },
 ];
