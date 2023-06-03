6338079bdfd90c4ae540dc77fc5ec374823c0a82
Laith Younes <laithyounesy@gmail.com>
2023-06-04 01:58:02 +0300

    added side configs modal

---
+++ src/components/layout/node-configurator.tsx
@@ -0,0 +1,62 @@
+import useFlowStore from "@pb/store/flow-builder.store";
+import React, { useCallback, useEffect, useState } from "react";
+
+import BaseInput from "../base-input";
+import { type PBNodeProps } from "../flow-builder/flow-builder.types";
+import InfoCard from "../info-card";
+
+type UpdateNodeConfig = (
+  key: string
+) => (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+
+const NodeConfigurator = ({ data, id }: PBNodeProps) => {
+  const { description, icon, label, nodeConfigs: initialNodeConfigs } = data;
+  const [nodeConfigs, setNodeConfigs] = useState(initialNodeConfigs);
+  const { updateNodeConfigs } = useFlowStore();
+
+  useEffect(() => {
+    setNodeConfigs(initialNodeConfigs);
+  }, [initialNodeConfigs]);
+
+  const updateNodeConfig: UpdateNodeConfig = useCallback(
+    (key) => (event) => {
+      setNodeConfigs((prevNodeConfigs) => ({
+        ...(prevNodeConfigs || {}),
+        [key]: event.target.value,
+      }));
+      updateNodeConfigs(id, key, event.target.value || "");
+    },
+    [id, updateNodeConfigs]
+  );
+
+  return (
+    <div className="h-full max-w-[32rem] flex-1 bg-base-100 px-4 py-9">
+      <InfoCard
+        containerCustomClasses="pb-6 items-start"
+        description={description}
+        icon={icon}
+        title={label}
+      />
+      <BaseInput
+        key={`message-${id}`}
+        containerCustomClasses="pt-6 pb-3"
+        inputType="textarea"
+        value={nodeConfigs?.message}
+        label="Message"
+        placeholder="Type in the question here..."
+        onChange={updateNodeConfig("message")}
+      />
+      <BaseInput
+        key={`answer-${id}`}
+        containerCustomClasses="pt-6 pb-3"
+        inputType="textarea"
+        label="Expected Answer"
+        placeholder="Leave it empty if nothing is expected from the user."
+        value={nodeConfigs?.expectedAnswer}
+        onChange={updateNodeConfig("expectedAnswer")}
+      />
+    </div>
+  );
+};
+
+export default NodeConfigurator;
--- src/components/layout/two-column.tsx
+++ src/components/layout/two-column.tsx
@@ -1,13 +1,19 @@
+import useFlowStore from "@pb/store/flow-builder.store";
 import type { TwoColumnLayoutProps } from "@pb/types";
 
-const TwoColumnLayout: React.FC<TwoColumnLayoutProps> = ({
-  leftContent,
-  rightContent,
-}) => {
+import type { SelectedNode } from "../flow-builder/flow-builder.types";
+import NodeConfigurator from "./node-configurator";
+
+const TwoColumnLayout: React.FC<TwoColumnLayoutProps> = ({ leftContent }) => {
+  const { nodes: initialNodes } = useFlowStore();
+  const selectedNode = initialNodes.find(
+    (node) => node.selected === true
+  ) as SelectedNode;
+
   return (
     <div className="row flex h-[calc(100vh-65px)] w-screen bg-base-200">
-      <div className="h-full w-[50vw]">{leftContent}</div>
-      <div className="h-full min-w-[50vw]">{rightContent}</div>
+      <div className="h-full flex-1">{leftContent}</div>
+      {selectedNode ? <NodeConfigurator {...selectedNode} /> : null}
     </div>
   );
 };
