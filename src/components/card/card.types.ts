63da10020ffcddeab8a4ae2ca7d97749886d098f
Laith Younes <laithyounesy@gmail.com>
2023-06-14 02:38:18 +0300

    added zod types

--- src/components/card/card.types.ts
+++ src/components/card/card.types.ts
@@ -1,5 +1,9 @@
-export type CardData = {
-  description: string;
-  flowID: string;
-  title: string;
-};
+import { z } from "zod";
+
+export const cardSchema = z.object({
+  description: z.string(),
+  flowID: z.string(),
+  title: z.string(),
+});
+
+export type CardData = z.infer<typeof cardSchema>;
--- src/components/chat/chat.types.ts
+++ src/components/chat/chat.types.ts
@@ -1,11 +1,17 @@
-export type MessageData = {
-  sender: "user" | "bot";
-  message: string;
-};
+import { z } from "zod";
 
-export type FlowMessages = {
-  id: string;
-  type: string;
-  message: string;
-  expectedAnswer: string;
-};
+export const messageSchema = z.object({
+  sender: z.enum(["user", "bot"]),
+  message: z.string(),
+});
+
+export type MessageData = z.infer<typeof messageSchema>;
+
+const flowMessagesSchema = z.object({
+  id: z.string(),
+  type: z.string(),
+  message: z.string(),
+  expectedAnswer: z.string(),
+});
+
+export type FlowMessages = z.infer<typeof flowMessagesSchema>;
