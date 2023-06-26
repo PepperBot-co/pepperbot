8403248e9401620b794dcdbc6441d9a3adc82ca8
Laith Younes <laithyounesy@gmail.com>
2023-06-26 16:34:22 +0300

    created theme hook

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,54 +1,12 @@
+import useThemeController from "@pb/hooks/useThemeController";
 import useFlowStore, { flowSelector } from "@pb/store/flow-builder.store";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { themeChange } from "theme-change";
 import { shallow } from "zustand/shallow";
 
-const themeOptions = [
-  { value: "", label: "Default" },
-  { value: "acid", label: "Acid" },
-  { value: "aqua", label: "Aqua" },
-  { value: "autumn", label: "Autumn" },
-  { value: "black", label: "Black" },
-  { value: "bumblebee", label: "Bumblebee" },
-  { value: "business", label: "Business" },
-  { value: "cmyk", label: "Cmyk" },
-  { value: "coffee", label: "Coffee" },
-  { value: "corporate", label: "Corporate" },
-  { value: "cupcake", label: "Cupcake" },
-  { value: "cyberpunk", label: "Cyberpunk" },
-  { value: "dark", label: "Dark" },
-  { value: "dracula", label: "Dracula" },
-  { value: "emerald", label: "Emerald" },
-  { value: "fantasy", label: "Fantasy" },
-  { value: "forest", label: "Forest" },
-  { value: "garden", label: "Garden" },
-  { value: "halloween", label: "Halloween" },
-  { value: "lemonade", label: "Lemonade" },
-  { value: "light", label: "Light" },
-  { value: "lofi", label: "Lofi" },
-  { value: "luxury", label: "Luxury" },
-  { value: "night", label: "Night" },
-  { value: "pastel", label: "Pastel" },
-  { value: "retro", label: "Retro" },
-  { value: "synthwave", label: "Synthwave" },
-  { value: "valentine", label: "Valentine" },
-  { value: "winter", label: "Winter" },
-  { value: "wireframe", label: "Wireframe" },
-];
-
 const Header: React.FC = () => {
-  const [theme, setTheme] = useState("");
   const { updateFlowMode, flowMode } = useFlowStore(flowSelector, shallow);
   const showChat = flowMode === 1;
-
-  useEffect(() => {
-    themeChange(false);
-  }, []);
-
-  const handleThemeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setTheme(event.target.value);
-  };
+  const { theme, themeOptions, setTheme } = useThemeController();
 
   return (
     <div className="navbar border-b border-base-300 bg-base-100">
@@ -66,7 +24,7 @@ const Header: React.FC = () => {
             className="select w-full max-w-sm border-base-300 focus:outline-none"
             data-choose-theme
             value={theme}
-            onChange={handleThemeChange}
+            onChange={(event) => setTheme(event.target.value)}
             id="theme-select"
           >
             <option disabled>Pick a theme</option>
